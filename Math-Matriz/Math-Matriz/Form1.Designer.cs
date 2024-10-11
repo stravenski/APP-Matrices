1a2b506a9c90068a596aad97eae51bd5c17bcadf
stravenski <132408592+stravenski@users.noreply.github.com>
2024-10-11 08:51:21 -0400

    Agregando funciones basicas

--- Math-Matriz/Math-Matriz/Form1.Designer.cs
+++ Math-Matriz/Math-Matriz/Form1.Designer.cs
@@ -28,13 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.txtDimenciones = new System.Windows.Forms.TextBox();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(60)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 50);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeight = 30;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.GridColor = System.Drawing.Color.Navy;
+            this.dataGridView1.Location = new System.Drawing.Point(194, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.DarkSlateBlue;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Size = new System.Drawing.Size(594, 377);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIgual.Location = new System.Drawing.Point(12, 364);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(91, 74);
+            this.btnIgual.TabIndex = 3;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // txtDimenciones
+            // 
+            this.txtDimenciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDimenciones.Location = new System.Drawing.Point(12, 61);
+            this.txtDimenciones.Name = "txtDimenciones";
+            this.txtDimenciones.Size = new System.Drawing.Size(91, 62);
+            this.txtDimenciones.TabIndex = 4;
+            this.txtDimenciones.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicacion.Location = new System.Drawing.Point(12, 265);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(91, 62);
+            this.btnMultiplicacion.TabIndex = 10;
+            this.btnMultiplicacion.Text = "x";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResta.Location = new System.Drawing.Point(12, 197);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(91, 62);
+            this.btnResta.TabIndex = 9;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(12, 129);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(91, 62);
+            this.btnSuma.TabIndex = 8;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(109, 61);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(79, 62);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(24)))), ((int)(((byte)(84)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txtDimenciones);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.TextBox txtDimenciones;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
--- Math-Matriz/Math-Matriz/Form1.cs
+++ Math-Matriz/Math-Matriz/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Math_Matriz
 {
@@ -16,5 +17,280 @@ namespace Math_Matriz
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<double[,]> matrices = new List<double[,]>();
+        private int rows;
+        private int cols;
+        private int operacionCounter = 0; // Contador para filas y columnas
+        private string operacionActual = ""; // Operación actual
+
+        private void InicializarDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            for (int i = 0; i < cols; i++)
+            {
+                dataGridView1.Columns.Add($"Col{i}", $"Columna {i + 1}");
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+        }
+
+        private void GuardarMatrix()
+        {
+            double[,] matrix = new double[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value != null &&
+                        double.TryParse(dataGridView1.Rows[i].Cells[j].Value.ToString(), out double value))
+                    {
+                        matrix[i, j] = value;
+                    }
+                }
+            }
+
+            matrices.Add(matrix);
+        }
+
+        private double[,] AddMatrices(List<double[,]> matrices)
+        {
+            var firstMatrix = matrices[0];
+
+            for (int i = 1; i < matrices.Count; i++)
+            {
+                if (firstMatrix.GetLength(0) != matrices[i].GetLength(0) ||
+                    firstMatrix.GetLength(1) != matrices[i].GetLength(1))
+                {
+                    MessageBox.Show("Las matrices deben tener las mismas dimensiones para sumar.");
+                    return null;
+                }
+
+                for (int row = 0; row < firstMatrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < firstMatrix.GetLength(1); col++)
+                    {
+                        firstMatrix[row, col] += matrices[i][row, col];
+                    }
+                }
+            }
+
+            return firstMatrix;
+        }
+
+        private double[,] SubtractMatrices(List<double[,]> matrices)
+        {
+            var firstMatrix = matrices[0];
+
+            for (int i = 1; i < matrices.Count; i++)
+            {
+                if (firstMatrix.GetLength(0) != matrices[i].GetLength(0) ||
+                    firstMatrix.GetLength(1) != matrices[i].GetLength(1))
+                {
+                    MessageBox.Show("Las matrices deben tener las mismas dimensiones para restar.");
+                    return null;
+                }
+
+                for (int row = 0; row < firstMatrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < firstMatrix.GetLength(1); col++)
+                    {
+                        firstMatrix[row, col] -= matrices[i][row, col];
+                    }
+                }
+            }
+
+            return firstMatrix;
+        }
+
+        private double[,] MultiplyMatrices(List<double[,]> matrices)
+        {
+            if (matrices.Count < 2)
+            {
+                MessageBox.Show("Se necesitan al menos 2 matrices para multiplicar.");
+                return null;
+            }
+
+            var result = matrices[0]; // Iniciar con la primera matriz
+
+            for (int i = 1; i < matrices.Count; i++)
+            {
+                var secondMatrix = matrices[i];
+
+                if (result.GetLength(1) != secondMatrix.GetLength(0))
+                {
+                    MessageBox.Show("El número de columnas de la primera matriz debe ser igual al número de filas de la segunda.");
+                    return null;
+                }
+
+                double[,] tempResult = new double[result.GetLength(0), secondMatrix.GetLength(1)];
+
+                for (int row = 0; row < tempResult.GetLength(0); row++)
+                {
+                    for (int col = 0; col < tempResult.GetLength(1); col++)
+                    {
+                        for (int k = 0; k < result.GetLength(1); k++)
+                        {
+                            tempResult[row, col] += result[row, k] * secondMatrix[k, col];
+                        }
+                    }
+                }
+
+                result = tempResult; // Actualizar el resultado
+            }
+
+            return result;
+        }
+
+        private void DisplayResult(double[,] result)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            int rows = result.GetLength(0);
+            int cols = result.GetLength(1);
+
+            for (int i = 0; i < cols; i++)
+            {
+                dataGridView1.Columns.Add($"Col{i}", $"Columna {i + 1}");
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                var rowValues = new object[cols];
+                for (int j = 0; j < cols; j++)
+                {
+                    rowValues[j] = result[i, j];
+                }
+                dataGridView1.Rows.Add(rowValues);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtDimenciones.Text, out double value))
+            {
+                if (operacionCounter == 0)
+                {
+                    // Guardar número de filas
+                    rows = (int)value;
+                    MessageBox.Show($"Filas definidas: {rows}");
+                }
+                else if (operacionCounter == 1)
+                {
+                    // Guardar número de columnas
+                    cols = (int)value;
+                    MessageBox.Show($"Columnas definidas: {cols}");
+
+                    // Inicializar DataGridView
+                    InicializarDataGridView();
+                    operacionCounter = -1; // Reiniciar para la siguiente entrada
+                }
+                operacionCounter++;
+                txtDimenciones.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un número válido.");
+            }
+        }
+
+        private void ClearDataGrid()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            operacionCounter = 0; // Reiniciar contador
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            operacionActual = "suma";
+            GuardarMatrix();
+            ClearDataGrid();
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            operacionActual = "resta";
+            GuardarMatrix();
+            ClearDataGrid();
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            operacionActual = "multiplicacion";
+            GuardarMatrix();
+            ClearDataGrid();
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            // Guarda la matriz actual antes de realizar la operación
+            GuardarMatrix();
+
+            if (matrices.Count < 2)
+            {
+                MessageBox.Show("Se necesitan al menos 2 matrices para realizar la operación.");
+                return;
+            }
+
+            double[,] resultado = null;
+
+            // Verifica qué operación se desea realizar
+            switch (operacionActual)
+            {
+                case "suma":
+                    resultado = AddMatrices(matrices);
+                    break;
+                case "resta":
+                    resultado = SubtractMatrices(matrices);
+                    break;
+                case "multiplicacion":
+                    resultado = MultiplyMatrices(matrices);
+                    break;
+                default:
+                    MessageBox.Show("Seleccione una operación válida.");
+                    return;
+            }
+
+            if (resultado != null)
+            {
+                DisplayResult(resultado);
+            }
+            else
+            {
+                MessageBox.Show("Error al realizar la operación.");
+            }
+
+            matrices.Clear(); // Limpiar matrices después de mostrar el resultado
+            operacionActual = ""; // Resetear operación
+        }
+
+        int x;
+        int y;
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                x = e.X;y = e.Y;
+            }
+            else
+            {
+                Left = Left + (e.X - x);
+                Top = Top + (e.Y - y);
+            }
+        }
+
     }
 }
